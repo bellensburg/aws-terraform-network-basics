6e1fb48a9a95d6f1d40c823e1c5b630fafb1ca27
bellensburg <bellensburg@gmail.com>
2026-06-02 21:48:38 +0200

    Create main.tf

---
+++ examples/simple_vpc/main.tf
@@ -0,0 +1,8 @@
+module "simple_vpc" {
+  source = "../.."
+
+  name               = "demo"
+  region             = "eu-west-1"
+  vpc_cidr           = "10.0.0.0/16"
+  public_subnet_cidr = "10.0.1.0/24"
+}
