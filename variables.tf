bd1bf163374928bfb38dd9ec8f29d839f35f0ef9
bellensburg <bellensburg@gmail.com>
2026-06-02 21:47:23 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "name" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
